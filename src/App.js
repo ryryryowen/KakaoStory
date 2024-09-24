51e34c9d40ff6039da1fe7e1ad93faae38ff64e8
MacUser <macuser@MacUserui-MacBookPro.local>
2024-09-24 18:25:32 +0900

    240924 Add Mixins & Variable Styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import styled from "styled-components";
 import "./App.css";
+import { GlobalStyles } from "./styles/GlobalStyles.styles";
+import { mixins } from "./styles/GlobalStyles.styles";
+
+const Title = styled.h4`
+  font-family: var(--pretendard);
+  font-weight: normal;
+  ${mixins.border({width: 10})};
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Title>테스트</Title>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/GlobalStyles.styles.js
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+  /* reset */
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ul, li{
+    list-style: none;
+  }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+
+  /* Variables */
+
+  :root{
+    --point-color: #ffe900;
+    --kakao-big-bold: "kakaoBigBold";
+    --kakao-big-regular: "kakaoBigRegular";
+    --kakao-small-bold: "kakaoSmallBold";
+    --kakao-small-regular: "kakaoSmallRegular";
+    --pretendard: "Pretendard Variable";
+  }
+`;
+
+export const mixins = {
+  flex: (flexProps) => `
+    display: flex;
+    justify-content: ${flexProps?.justify ?? "center"};
+    align-items: ${flexProps?.align ?? "center"};
+    flex-direction: ${flexProps?.direction ?? "row"};
+    gap: ${flexProps?.gap ?? "14"}px;
+  `,
+  border: (borderProps) => `
+    border: ${borderProps?.width ?? "1"}px ${borderProps?.type ?? "solid"} ${
+    borderProps?.color ?? "red"
+  };
+  `,
+  focus: () => `
+  &:focus{
+    outline:none;
+  }
+  `,
+};
+
