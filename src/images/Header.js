ddc895a332468f01976e0ae408881164a28a6296
MacUser <macuser@MacUserui-MacBookPro.local>
2024-09-26 18:50:14 +0900

    240926 header plus

--- src/images/Header.js
+++ src/images/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
-import image from "../../images/kakaoLogo.png";
+import image from "./images/kakaoLogo.png";
 
 const HeaderHeader = styled.main`
   width: 100%;
@@ -25,12 +25,6 @@ const SearchBarHeader = styled.input`
   border: none;
   border-radius: 24px;
   background: #fbfbfb;
-  position: relative;
-`;
-
-const SearchBarHeaderValue = styled.div`
-  position: absolute;
-  left: 230px;
 `;
 
 const AddStoryHeader = styled.button`
@@ -56,27 +50,25 @@ const LeftIconHeader = styled.div`
 `;
 
 function Header() {
-  return (
-    <HeaderHeader>
-      <KakaoLogo />
-      <SearchBarHeader type="text" id="searchBarHeader" />
-      <SearchBarHeaderValue>
-        <i className="fa-solid fa-magnifying-glass"></i>검색
-      </SearchBarHeaderValue>
-      <AddStoryHeader>오늘의 스토리를 들려주세요.</AddStoryHeader>
-      <LeftIconHeader>
-        <button>
-          <i class="fa-solid fa-user-group"></i>
-        </button>
-        <button>
-          <i class="fa-regular fa-bell"></i>
-        </button>
-        <button>
-          <i class="fa-regular fa-circle-user"></i>
-        </button>
-      </LeftIconHeader>
-    </HeaderHeader>
-  );
+  <HeaderHeader>
+    <KakaoLogo />
+    <SearchBarHeader type="text" id="searchBarHeader" />
+    <div>
+      <i className="fa-solid fa-magnifying-glass"></i>검색
+    </div>
+    <AddStoryHeader>오늘의 스토리를 들려주세요.</AddStoryHeader>
+    <LeftIconHeader>
+      <button>
+        <i class="fa-solid fa-user-group"></i>
+      </button>
+      <button>
+        <i class="fa-regular fa-bell"></i>
+      </button>
+      <button>
+        <i class="fa-regular fa-circle-user"></i>
+      </button>
+    </LeftIconHeader>
+  </HeaderHeader>;
 }
 
 export default Header;
